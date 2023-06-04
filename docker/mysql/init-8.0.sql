6c5d093cc440e97a8af58886feae5222232208fc
Juan Carrizalez <sitgem@gmail.com>
2023-06-04 14:53:35 -0300

    change commad

--- docker/mysql/init-8.0.sql
+++ docker/mysql/init-8.0.sql
@@ -1,3 +1,9 @@
+-- create the users for each database
+-- CREATE USER IF NOT EXISTS 'develop'@'%' IDENTIFIED BY '123456';
+-- GRANT ALL PRIVILEGES ON * . * TO 'develop'@'%';
+
+FLUSH PRIVILEGES;
+
 -- create the databases
 CREATE DATABASE IF NOT EXISTS ocr_test;
 CREATE DATABASE IF NOT EXISTS ocr;
@@ -6,9 +12,3 @@ CREATE DATABASE IF NOT EXISTS ocr;
 -- SET GLOBAL log_bin_trust_function_creators = 1;
 -- SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';
 -- SET SESSION sql_mode = 'NO_ENGINE_SUBSTITUTION';
-
--- create the users for each database
--- CREATE USER IF NOT EXISTS 'develop'@'%' IDENTIFIED BY '123456';
-GRANT ALL PRIVILEGES ON * . * TO 'develop'@'%';
-
-FLUSH PRIVILEGES;
